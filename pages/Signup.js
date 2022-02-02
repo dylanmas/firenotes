3c0d39afdbbe237cd32aa00b5aa12abcc1f3e695
dylanmas <dylanmas687@gmail.com>
2022-02-02 11:03:11 -0600

    Beautified the login screen

--- pages/Signup.js
+++ pages/Signup.js
@@ -7,16 +7,50 @@ const Styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  }
-})
+    backgroundColor: "#FFEAB5",
+  },
+});
 
 const Signup = () => {
-  const [userName, SetUserName] = useState('')
+  const [userName, SetUserName] = useState("");
   return (
     <View style={Styles.Container}>
       <TextInput
-        style={{ height: 40 , borderRadius: 10, backgroundColor: "#ff32ee", width: "100%", padding: 10, color: "white", fontSize: 20}}
+        style={{
+          height: 40,
+          borderRadius: 10,
+          backgroundColor: "#FFFCE0",
+          width: "90%",
+          padding: 10,
+          color: "#504801",
+          fontSize: 20,
+          shadowColor: "#171717",
+          shadowOffset: { width: 0, height: 4 },
+          shadowOpacity: 0.2,
+          shadowRadius: 3,
+          marginBottom: 10,
+        }}
         placeholder="Username"
+        defaultValue={userName}
+        onChangeText={(e) => SetUserName(e)}
+      />
+
+      <TextInput
+        style={{
+          height: 40,
+          borderRadius: 10,
+          backgroundColor: "#FFFCE0",
+          width: "90%",
+          padding: 10,
+          color: "#504801",
+          fontSize: 20,
+          shadowColor: "#171717",
+          shadowOffset: { width: 0, height: 4 },
+          shadowOpacity: 0.2,
+          shadowRadius: 3,
+          marginBottom: 10,
+        }}
+        placeholder="Password"
         defaultValue={userName}
         onChangeText={(e) => SetUserName(e)}
       />
