46a6409f83b1acf0aa88df46d829207d7e8e2725
dylanmas <dylanmas687@gmail.com>
2022-02-04 10:58:16 -0600

    Added sign up button to log in page

--- pages/Signup.js
+++ pages/Signup.js
@@ -29,14 +29,26 @@ const Signup = () => {
       </Text>
 
       <InputField value="Email" />
-      <InputField value="Password" onTextChange={(e) => console.log(e)}/>
+      <InputField value="Password" onTextChange={(e) => console.log(e)} />
 
       <Button
         //onPress={logIn}
         //textStyle={{ fontFamily: "AvenirNextCondensed-Medium" }}
-        title="Sign up"
+        title="Sign in"
         backgroundColor="black"
       />
+
+      <View style={{ flexDirection:'row', flexWrap:'wrap'}}>
+        <Text
+          style={{
+            fontSize: 20,
+            fontFamily: "AvenirNextCondensed-Medium",
+          }}
+        >
+          No account? {<Text style={{fontWeight: "600", fontStyle: "italic"}}>Create one here.</Text>}
+        </Text>
+
+      </View>
     </View>
   );
 };
