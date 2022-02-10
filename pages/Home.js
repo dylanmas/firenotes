d820ecd8bf53f1ba1d46707b036b2aee473fe097
dylanmas <dylanmas687@gmail.com>
2022-02-10 10:56:02 -0600

    Started basic Home.js styling

--- pages/Home.js
+++ pages/Home.js
@@ -1,8 +1,36 @@
-import React from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  Touchable,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
+import { app } from "../firebase/config";
+import InputField from "../components/InputField";
+import { NavigationContainer } from "@react-navigation/native";
+
+import logo from "../assets/logo.png";
+const Styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFEAB5",
+  },
+});
+
 function Home() {
   return (
-    <View></View>
+    <View>
+     <Text>Home</Text>
+    </View>
+  <View style={Styles.Container}>
+    <Text>Home</Text>
+  </View>
   );
 }
 
