5cae74669be41d30a23161cdab3200e3bae28212
dylanmas <dylanmas687@gmail.com>
2022-02-16 10:48:24 -0600

    Progress on home ui

--- pages/Home.js
+++ pages/Home.js
@@ -25,23 +25,61 @@ const Styles = StyleSheet.create({
 function Home() {
   return (
     <View style={Styles.Container}>
-      <Text style={{
-        fontSize: 45,
-        fontWeight: "bold",
-        backgroundColor: "red",
-        width: "100%",
-        textAlign: "center"
-      }}>Home</Text>
+      <Text
+        style={{
+          fontFamily: "AvenirNextCondensed-Medium",
+          fontSize: 30,
+          fontWeight: "600",
+          width: "100%",
+          top: 20,
+          textAlign: "center",
+        }}
+      >
+        Home
+      </Text>
 
-      <View style={{
-        backgroundColor: "blue",
-        width: "100%",
-        height: "95%",
-      }}>
-        <View style={{backgroundColor: "green", marginHorizontal: 10, width: "100%",
-       height: "100%"}}>
-
-        </View>
+      <View
+        style={{
+          width: "100%",
+          height: "95%",
+          alignItems: "center",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            fontWeight: "600",
+            color: "white",
+            fontSize: 25,
+            marginTop: 20,
+            fontFamily: "AvenirNextCondensed-Medium",
+            margin: 10,
+            borderRadius: 5,
+            backgroundColor: "#FFFCE0",
+            width: "90%",
+            height: "30%",
+            textAlign: "center",
+            display: "flex",
+            justifyContent: "center",
+            shadowColor: "#171717",
+            shadowOffset: { width: 0, height: 4 },
+            shadowOpacity: 0.2,
+            shadowRadius: 3,
+          }}
+        >
+          <Text
+            style={{
+              color: "#504801",
+              textAlign: "center",
+              fontFamily: "AvenirNextCondensed-Medium",
+              fontWeight: "600",
+              fontSize: 20,
+              marginVertical: 5,
+            }}
+          ></Text>
+        </TouchableOpacity>
+        <View
+          style={{ marginHorizontal: 10, width: "100%", height: "100%" }}
+        ></View>
       </View>
     </View>
   );
